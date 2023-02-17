663e553d6ceb315d6f70f72503aab794bd54f5ba
Facundo Medero <facundomedero26@gmail.com>
2023-02-17 13:06:29 -0300

    Include parameters from script

--- age_calculator.py
+++ age_calculator.py
@@ -1,14 +1,28 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-## Age Calculator using Python
+import argparse
+import datetime
 
+## Age Calculator using Python
+print("Ingrese su año, mes y dia de nacimiento, en ese orden y separados por un espacio. Por ejemplo: 2022 12 18")
 def ageCalculator(y, m, d):
-    import datetime
     today = datetime.datetime.now().date()
     dob = datetime.date(y, m, d)
     age = int((today-dob).days / 365.25)
     print(age)
 
-# Format: y=year m=month d=day    
-ageCalculator(2009 , 10, 19)
+# Define the command line arguments
+parser = argparse.ArgumentParser(description='Age Calculator using Python')
+parser.add_argument('year', type=int, help='the year of birth')
+parser.add_argument('month', type=int, help='the month of birth')
+parser.add_argument('day', type=int, help='the day of birth')
+
+# Parse the command line arguments
+args = parser.parse_args()
+year = args.year
+month = args.month
+day = args.day
+
+# Calculate and print the age
+ageCalculator(year, month, day)
